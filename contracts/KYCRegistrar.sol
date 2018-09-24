b4f635dcf7593e389fe38dc382da7dd369aecfc1
iamdefinitelyahuman <ben@wolfcrypto.net>
2018-09-24 13:46:27 +0300

    Added events.

--- contracts/KYCRegistrar.sol
+++ contracts/KYCRegistrar.sol
@@ -22,6 +22,13 @@ contract KYCRegistrar {
   mapping (bytes32 => Entity) registry;
   mapping (address => bytes32) idMap;
   mapping (bytes32 => Investor) investorData;
+
+  event NewInvestor (bytes32 id, uint16 country, uint8 rating, uint40 expires);
+  event NewIssuer (bytes32 id, uint16 country);
+  event NewExchange (bytes32 id, uint16 country);
+  event EntityRestriction (bytes32 id, uint8 type_, bool restricted);
+  event NewRegisteredAddress(bytes32 id, uint8 type_, address addr);
+  event UnregisteredAddress(bytes32 id, uint8 type_, address addr);
   
   modifier onlyOwner () {
     require (msg.sender == owner);
@@ -45,14 +52,17 @@ contract KYCRegistrar {
     _addEntity(_id, 1, _country);
     investorData[_id].rating = _rating;
     investorData[_id].expires = _expires;
+    emit NewInvestor(_id, _country, _rating, _expires);
   }
   
   function addIssuer(bytes32 _id, uint16 _country) public onlyOwner {
     _addEntity(_id, 2, _country);
+    emit NewIssuer(_id, _country);
   }
   
   function addExchange(bytes32 _id, uint16 _country) public onlyOwner {
     _addEntity(_id, 3, _country);
+    emit NewExchange(_id, _country);
   }
   
   function _addEntity(bytes32 _id, uint8 _type, uint16 _country) internal {
@@ -65,14 +75,19 @@ contract KYCRegistrar {
   function setRestricted (bytes32 _id, bool _restricted)  public onlyOwner {
     require (registry[_id].type_ != 0);
     registry[_id].restricted = _restricted;
+    emit EntityRestriction(_id, registry[_id].type_, _restricted);
   }
 
   function registerAddress (address _addr, bytes32 _id) public onlyOwner {
     require (idMap[_addr] == 0);
+    require (registry[_id].type_ != 0);
     idMap[_addr] = _id;
+    emit NewRegisteredAddress(_id, registry[_id].type_, _addr);
   }
   
   function unregisterAddress (address _addr) public onlyOwner {
+    require (idMap[_addr] != 0);
+    emit UnregisteredAddress(idMap[_addr], registry[idMap[_addr]].type_, _addr);
     delete idMap[_addr];
   }
 
