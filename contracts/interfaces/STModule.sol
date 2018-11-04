8210b68b0edba424c33df868ac8969e2f6e3aeb3
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-04 13:38:17 +0000

    max line length

--- contracts/interfaces/STModule.sol
+++ contracts/interfaces/STModule.sol
@@ -7,19 +7,81 @@ interface IBaseModule {
 }
 
 interface ISTModule {
-	function checkTransfer(address[2] _addr, bytes32 _authId, bytes32[2] _id, uint8[2] _rating, uint16[2] _country, uint256 _value) external view returns (bool);
-	function transferTokens(address[2] _addr, bytes32[2] _id, uint8[2] _rating, uint16[2] _country, uint256 _value) external view returns (bool);
-	function balanceChanged(address _addr, bytes32 _id, uint8 _rating, uint16 _country, uint256 _old, uint256 _new) external returns (bool);
-	function getBindings() external view returns (bool, bool, bool);
+	function checkTransfer(
+		address[2] _addr,
+		bytes32 _authId,
+		bytes32[2] _id,
+		uint8[2] _rating,
+		uint16[2] _country,
+		uint256 _value
+	)
+		external
+		view
+		returns (bool);
+	function transferTokens(
+		address[2] _addr,
+		bytes32[2] _id,
+		uint8[2] _rating,
+		uint16[2] _country,
+		uint256 _value
+	)
+		external
+		view
+		returns (bool);
+	function balanceChanged(
+		address _addr,
+		bytes32 _id,
+		uint8 _rating,
+		uint16 _country,
+		uint256 _old,
+		uint256 _new
+	)
+		external
+		returns (bool);
+	function getBindings() external view returns (
+		bool _checkTransfer,
+		bool _transferTokens, 
+		bool _balanceChanged);
 	function token() external returns (address);
 	function owner() external view returns (address);
 }
 
 interface IIssuerModule {
-	function checkTransfer(address _token, bytes32 _authId, bytes32[2] _id, uint8[2] _rating, uint16[2] _country, uint256 _value) external view returns (bool);
-	function transferTokens(address _token, bytes32[2] _id, uint8[2] _rating, uint16[2] _country, uint256 _value) external view returns (bool);
-	function balanceChanged(address _token, bytes32 _id, uint8 _rating, uint16 _country, uint256 _old, uint256 _new) external returns (bool);
-	function getBindings() external view returns (bool, bool, bool);
+	function checkTransfer(
+		address _token,
+		bytes32 _authId,
+		bytes32[2] _id,
+		uint8[2] _rating,
+		uint16[2] _country,
+		uint256 _value
+	)
+		external
+		view
+		returns (bool);
+	function transferTokens(
+		address _token,
+		bytes32[2] _id,
+		uint8[2] _rating,
+		uint16[2] _country,
+		uint256 _value
+	)
+		external
+		view
+		returns (bool);
+	function balanceChanged(
+		address _token,
+		bytes32 _id,
+		uint8 _rating,
+		uint16 _country,
+		uint256 _old,
+		uint256 _new
+	)
+		external
+		returns (bool);
+	function getBindings() external view returns (
+		bool _checkTransfer,
+		bool _transferTokens, 
+		bool _balanceChanged);
 	function issuer() external returns (address);
 	function owner() external view returns (address);
 }
