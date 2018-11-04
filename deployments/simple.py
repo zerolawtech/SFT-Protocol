47a83be24ba3f94b583c5a66049dd6050650653f
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-04 16:11:25 +0000

    editorconfig

--- deployments/simple.py
+++ deployments/simple.py
@@ -1,8 +1,11 @@
 
-
+import itertools
 
 def deploy(network, accounts):
-    network.deploy('KYCRegistrar', [accounts[0]], 0)
+    kyc = network.deploy('KYCRegistrar', [accounts[0]], 0)
     issuer = network.deploy('IssuingEntity', [accounts[1]], 1, {'from': accounts[1]})
     token = network.deploy('SecurityToken', issuer.address, "Test Token", "TST", 1000000, {'from':accounts[1]})
-    issuer.addToken(token.address, {'from':accounts[1]})
+    issuer.addToken(token.address)
+    for count,country,rating in [(c,i[0],i[1]) for c,i in enumerate(itertools.product([1,2,3], [1,2]))]:
+    kyc.addInvestor(b"investor"+str(count), country, 0, rating, 9999999999, accounts[count])
+    
