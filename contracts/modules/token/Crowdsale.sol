885b3ded76da09516b643adccf771abc435d437d
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-04 15:58:10 +0000

    Crowdsale module - closes #28

--- contracts/modules/token/Crowdsale.sol
+++ contracts/modules/token/Crowdsale.sol
@@ -17,21 +17,49 @@ contract Crowdsale is STModuleBase {
 	uint256 public ethFiatRate;
 	uint256 public tokenFiatRate;
 
-	uint256 public maxTokensSold;
-	uint256 public maxFiatRaised;
-	uint256 public tokensSold;
-	uint256 public fiatRaised;
+	uint256 public tokensMax;
+	uint256 public fiatMax;
+	uint256 public tokens;
+	uint256 public fiat;
 
+	uint256[] public bonusPct;
+	uint64[] public bonusTimes;
 
 	constructor (
 		address _token,
 		address _issuer,
-		address _receiver
+		address _receiver,
+		uint64 _start,
+		uint64 _finish,
+		uint256 _ethRate,
+		uint256 _tokenRate,
+		uint256 _fiatMax,
+		uint256 _tokensMax,
+		uint256[] _bonusPct,
+		uint64[] _bonusTimes
 	)
 		STModuleBase(_token, _issuer)
 		public
 	{
+		require (_fiatMax > 0 || _tokensMax > 0);
+		require (_ethRate > 0 && _tokenRate > 0);
+		require (_finish > now);
+		require (_bonusPct.length == _bonusTimes.length);
+		if (_bonusTimes.length > 0) {
+			require(_bonusTimes[0] <= _start);
+			for (uint256 i = 1; i < _bonusPct.length; i++) {
+				require(_bonusTimes[i] > _bonusTimes[i-1]);
+			}
+		}
 		receiver = _receiver;
+		crowdsaleStart = _start;
+		crowdsaleFinish = _finish;
+		ethFiatRate = _ethRate;
+		tokenFiatRate = _tokenRate;
+		fiatMax = _fiatMax;
+		tokensMax = _tokensMax;	
+		bonusTimes = _bonusTimes;
+		bonusPct = _bonusPct;
 	}
 
 	function () public payable {
@@ -40,13 +68,19 @@ contract Crowdsale is STModuleBase {
 		require (crowdsaleCompleted == 0);
 		require (msg.value > 0);
 		uint256 _fiat = msg.value.mul(ethFiatRate).div(1 ether);
-		_fiat = _fiat.sub(_checkExcess(fiatRaised, maxFiatRaised, _fiat));
+		_fiat = _fiat.sub(_checkExcess(fiat, fiatMax, _fiat));
 		uint256 _tokens = _fiat.div(tokenFiatRate);
-		_tokens = _tokens.sub(_checkExcess(tokensSold, maxTokensSold, _tokens));
+		for (uint256 i = bonusPct.length; i < 0; i--) {
+			if (bonusTimes[i] <= now) {
+				_tokens = _tokens.mul(bonusPct[i].add(100)).sub(100);
+				break;
+			}
+		}
+		_tokens = _tokens.sub(_checkExcess(tokens, tokensMax, _tokens));
 		receiver.transfer(address(this).balance);
 		token.transferFrom(address(issuer), msg.sender, _tokens);
-		fiatRaised = fiatRaised.add(_fiat);
-		tokensSold = tokensSold.add(_tokens);
+		fiat = fiat.add(_fiat);
+		tokens = tokens.add(_tokens);
 	}
 
 	function _checkExcess(
@@ -66,19 +100,41 @@ contract Crowdsale is STModuleBase {
 		return _excess;
 	}
 
-	function manualSale(address _to, uint256 _tokens, uint256 _fiat) external onlyIssuer {
-		require (tokensSold.add(_tokens) <= maxTokensSold);
-		require (fiatRaised.add(_fiat) <= maxFiatRaised);
+	function manualSale(
+		address _to,
+		uint256 _tokens,
+		uint256 _fiat
+	)
+		external
+		onlyIssuer
+	{
+		require (tokens.add(_tokens) <= tokensMax);
+		require (fiat.add(_fiat) <= fiatMax);
 		token.transferFrom(address(issuer), _to, _tokens);
-		tokensSold = tokensSold.add(_tokens);
-		fiatRaised = fiatRaised.add(_fiat);
-		if (tokensSold == maxTokensSold || fiatRaised == maxFiatRaised) {
+		tokens = tokens.add(_tokens);
+		fiat = fiat.add(_fiat);
+		if (tokens == tokensMax || fiat == fiatMax) {
 			crowdsaleCompleted = uint64(now);
 		}
 	}
 
 	function setEthFiatRate(uint256 _rate) external onlyIssuer {
 		ethFiatRate = _rate;
+	}
+
+	function isOpen() public view returns (bool) {
+		return (
+			now > crowdsaleStart &&
+			now < crowdsaleFinish &&
+			crowdsaleCompleted == 0
+		);
+	}
+
+	function canParticipate(address _addr) external view returns (bool) {
+		if (!isOpen()) {
+			return false;
+		}
+		return token.checkTransfer(address(issuer), _addr, 1);
 	}
 
 	function checkTransfer(
