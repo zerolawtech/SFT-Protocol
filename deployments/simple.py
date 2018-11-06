4a4cae62bc3f0aa2c6c0e648af51a829575eab58
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-06 04:53:52 +0000

    updated based on brownie changes

--- deployments/simple.py
+++ deployments/simple.py
@@ -3,9 +3,9 @@
 import itertools
 
 def deploy(network, accounts):
-    kyc = network.deploy('KYCRegistrar', [accounts[0]], 0)
-    issuer = network.deploy('IssuingEntity', [accounts[1]], 1, {'from': accounts[1]})
-    token = network.deploy('SecurityToken', issuer.address, "Test Token", "TST", 1000000, {'from':accounts[1]})
+    kyc = accounts[0].deploy('KYCRegistrar', [accounts[0]], 0)
+    issuer = accounts[1].deploy('IssuingEntity', [accounts[1]], 1)
+    token = accounts[1].deploy('SecurityToken', issuer.address, "Test Token", "TST", 1000000)
     issuer.addToken(token.address)
     issuer.addRegistrar(kyc.address)
     for count,country,rating in [(c,i[0],i[1]) for c,i in enumerate(itertools.product([1,2,3], [1,2]), start=2)]:
