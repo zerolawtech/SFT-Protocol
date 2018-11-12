d8736493717c04a0ee79d79946823b9355b3a544
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-12 04:22:02 +0200

    Updated mintburn_dividend test to fit brownie upgrades

--- tests/mintburn_dividend.py
+++ tests/mintburn_dividend.py
@@ -9,60 +9,90 @@ def mintburn_setup():
     global issuer, token, mint
     issuer = IssuingEntity[0]
     token = SecurityToken[0]
-    mint = accounts[1].deploy(MintBurnModule, issuer.address)
-    assert issuer.revert("attachModule", issuer.address,
-                         mint.address, {'from':accounts[2]}), (
-                             "Account 2 was able to attach")
-    issuer.attachModule(issuer.address, mint.address)
-    assert issuer.revert("attachModule", issuer.address, mint.address), (
-        "Was able to attach module twice")
+    mint = check.confirms(
+        accounts[1].deploy,
+        (MintBurnModule, issuer.address),
+        "Could not deploy MintBurn")
+    check.reverts(
+        issuer.attachModule,
+        (issuer.address,mint.address, {'from':accounts[2]}),
+        "Account 2 was able to attach MintBurn module")
+    check.confirms(
+        issuer.attachModule,
+        (issuer.address, mint.address),
+        "Issuer could not attach MintBurn module")
+    check.reverts(
+        issuer.attachModule,
+        (issuer.address, mint.address),
+        "Issuer was able to attach module twice")
 
 def mintburn_mint():
     '''MintBurn: mint tokens'''
-    mint.mint(token.address, 1000000)
-    assert token.balanceOf(issuer.address) == 2000000, "Issuer balance is wrong"
-    assert mint.revert("mint", token.address, 1, {'from':accounts[2]}), (
+    check.confirms(mint.mint, (token.address, 1000000), "Unable to mint")
+    check.equal(token.balanceOf(issuer.address), 2000000, "Issuer balance is wrong")
+    check.reverts(
+        mint.mint,
+        (token.address, 1, {'from':accounts[2]}),
         "Account 2 was able to mint")
 
 def mintburn_transfer():
     '''MintBurn: transfer tokens while attached'''
-    token.transfer(accounts[2],10000)
-    token.transfer(accounts[1],10000, {'from':accounts[2]})
+    check.confirms(token.transfer, (accounts[2],10000), "Unable to send tokens")
+    check.confirms(token.transfer,(accounts[1],10000, {'from':accounts[2]}), "Unable to send tokens")
 
 def mintburn_burn():
     '''MintBurn: burn tokens'''
-    mint.burn(token.address, 1000000)
-    assert token.balanceOf(issuer.address) == 1000000, "Issuer balance is wrong"
-    assert mint.revert("burn", token.address, 1000000, {'from':accounts[2]}), (
+    check.confirms(mint.burn,(token.address, 1000000),"Unable to burn")
+    check.equal(token.balanceOf(issuer.address), 1000000, "Issuer balance is wrong")
+    check.reverts(
+        mint.burn, (token.address, 1000000, {'from':accounts[2]}),
         "Account 2 was able to burn")
-    assert mint.revert("burn", token.address, 2000000), (
+    check.reverts(
+        mint.burn, (token.address, 2000000),
         "Was able to burn more tokens than currently exist")
 
 def mintburn_detach():
     '''MintBurn: detach module'''
-    assert issuer.revert("detachModule", issuer.address,
-                         mint.address, {'from':accounts[2]}), (
-                             "Was able to detach from account 2")
-    assert issuer.revert("detachModule", token.address, mint.address), (
-        "Was able to detach from token instead of issuer"
-    )
-    issuer.detachModule(issuer.address, mint.address)
+    check.reverts(
+        issuer.detachModule,
+        (issuer.address, mint.address, {'from':accounts[2]}),
+        "Was able to detach from account 2")
+    check.reverts(
+        issuer.detachModule,
+        (token.address, mint.address),
+        "Was able to detach from token instead of issuer")
+    check.confirms(
+        issuer.detachModule,
+        (issuer.address, mint.address),
+        "Unable to detach module")
 
 def mintburn_final():
     '''MintBurn: attach and detach once more'''
-    issuer.attachModule(issuer.address, mint.address)
-    issuer.detachModule(issuer.address, mint.address)
+    check.confirms(
+        issuer.attachModule,
+        (issuer.address, mint.address),
+        "Unable to attach module")
+    check.confirms(
+        issuer.detachModule,
+        (issuer.address, mint.address),
+        "Unable to attach module")
 
 def dividend_setup():
     '''Dividend: deploy and attach'''
     global dividend_time, dividend
     dividend_time = int(time.time()+3)
-    dividend = accounts[1].deploy(DividendModule, token.address, issuer.address,
-                            dividend_time)
-    assert issuer.revert("attachModule", token.address,
-                         dividend.address, {'from':accounts[2]}), (
-                             "Account 2 was able to attach")
-    issuer.attachModule(token.address, dividend.address)
+    dividend = check.confirms(
+        accounts[1].deploy,
+        (DividendModule, token.address, issuer.address, dividend_time),
+        "Should have deployed Dividend module")
+    check.reverts(
+        issuer.attachModule,
+        (token.address, dividend.address, {'from':accounts[2]}),
+        "Account 2 was able to attach")
+    check.confirms(
+        issuer.attachModule,
+        (token.address, dividend.address),
+        "Unable to attach module")
 
 def dividend_transfer():
     '''Dividend: transfer tokens before claim time'''
@@ -73,7 +103,7 @@ def dividend_transfer():
     token.transfer(accounts[5], 100, {'from':accounts[4]})
     token.transfer(accounts[6], 900)
     token.transferFrom(accounts[6], accounts[7], 600, {'from':accounts[1]})
-    assert token.circulatingSupply() == 2000, "Circulating supply is wrong"
+    check.equal(token.circulatingSupply(), 2000, "Circulating supply is wrong")
 
 def dividend_mint():
     '''Dividend: attach MintBurn, mint and burn tokens'''
@@ -91,26 +121,35 @@ def dividend_transfer2():
 
 def dividend_issue():
     '''Dividend: issue the dividend'''
-    assert dividend.revert("issueDividend", 100,
-                           {'from':accounts[2], 'value':1e19}), (
-                               "Dividend was successfully issued by account 2")
-    assert dividend.revert("issueDividend", 100), (
+    check.reverts(
+        dividend.issueDividend,
+        (100, {'from':accounts[2], 'value':1e19}),
+        "Dividend was successfully issued by account 2")
+    check.reverts(dividend.issueDividend, (100,),
         "Was able to issue a dividend without sending any eth")
-    dividend.issueDividend(100, {'value':2e19})
-    assert dividend.revert("issueDividend", 100, {'value':1e19}), (
+    check.confirms(
+        dividend.issueDividend,
+        (100, {'value':2e19}),
+        "Unable to issue Dividend")
+    check.reverts(
+        dividend.issueDividend,
+        (100, {'value':1e19}),
         "Was able to call issueDividend twice")
 
 def dividend_claim():
     '''Dividend: claim dividends'''
     blank = "0x"+("0"*40)
 
-    assert dividend.revert("claimDividend",accounts[1]), "Issuer was able to claim"
-    dividend.claimDividend(accounts[8])
+    check.reverts(
+        dividend.claimDividend,
+        (accounts[1],),
+        "Issuer was able to claim")
+    #dividend.claimDividend(accounts[8])
     for i,final in enumerate([int(4e18), int(2e18), int(4e18), int(1e18), int(3e18), int(6e18)], start=2):
         balance = accounts[i].balance()
         dividend.claimDividend(accounts[i])
-        assert accounts[i].balance() == balance+final, "Dividend payout wrong: {}".format(i)
-        assert dividend.revert("claimDividend",accounts[i]), "Able to claim twice"
+        check.equal(accounts[i].balance(), balance+final, "Dividend payout wrong: {}".format(i))
+        check.reverts(dividend.claimDividend,(accounts[i],), "Able to claim twice")
 
 def dividend_close():
     '''Dividend: close dividends'''
