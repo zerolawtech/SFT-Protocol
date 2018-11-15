9b8389888ecc36ce3b292999f27f8c4da4ce8ff7
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2018-11-15 16:33:59 +0200

    Custodian natspec

--- contracts/Custodian.sol
+++ contracts/Custodian.sol
@@ -3,13 +3,27 @@ pragma solidity ^0.4.24;
 import "./SecurityToken.sol";
 import "./components/MultiSig.sol";
 
+/**
+	@title Custodian Contract
+	@dev
+		This is a bare-bones implementation of a custodian contract,
+		it should be expanded upon depending on the specific needs
+		of the owner.
+ */
 /// @title Custodian Contract
+/// @dev
 contract Custodian is MultiSigMultiOwner {
 
 	string public name;
 	bytes32 public id;
 	mapping (address => bool) public addresses;
 
+	/**
+		@notice Custodian constructor
+		@param _name Human-readable name of custodian
+		@param _owners Array of addresses to associate with owner
+		@param _threshold multisig threshold for owning authority
+	 */
 	constructor(
 		string _name,
 		address[] _owners,
@@ -22,6 +36,16 @@ contract Custodian is MultiSigMultiOwner {
 		id = keccak256(abi.encodePacked(address(this)));
 	}
 
+	/**
+		@notice Custodian transfer function
+		@dev
+			Addresses associated to the custodian cannot directly hold tokens,
+			so they must use this transfer function to move them.
+		@param _token Address of the token to transfer
+		@param _to Address of the recipient
+		@param _value Amount to transfer
+		@return bool success
+	 */
 	function transfer(
 		address _token,
 		address _to,
